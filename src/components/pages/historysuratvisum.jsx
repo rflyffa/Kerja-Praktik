fcd99527ae3ae3da6615ec89d84a28c362559725
fadelhafidz <fadellataa@gmail.com>
2024-11-21 01:28:48 +0700

    update

--- src/components/pages/historysuratvisum.jsx
+++ src/components/pages/historysuratvisum.jsx
@@ -372,9 +372,13 @@ const Historysuratvisum = ({ userRole }) => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-200 to-indigo-400 py-8 px-4 sm:px-6 lg:px-8">
             <div className="mt-20 max-w-7xl mx-auto">
-                <h2 className="text-4xl font-extrabold text-gray-900 mb-8 text-center tracking-tight">
-                    History Surat Visum
-                </h2>
+            <div className="flex justify-center items-center">
+  <h2 className="text-5xl font-helvetica-extrabold text-gray-900 mb-4 animate-fade-in">
+    History Surat Visum
+  </h2>
+</div>
+
+
                 <div className="mb-6 flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 sm:space-x-4">
                     <div className="relative flex items-center w-full sm:w-auto">
                         <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
